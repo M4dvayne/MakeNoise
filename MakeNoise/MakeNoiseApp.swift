19c81d9428a689458c29a6ebdac0f00aa3ecdaea
M4dvayne <rusted2192@gmail.com>
2022-08-02 21:05:14 +0300

    Initial Commit

---
+++ MakeNoise/MakeNoiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakeNoiseApp.swift
+//  MakeNoise
+//
+//  Created by Вячеслав Кремнев on 02.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct MakeNoiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
